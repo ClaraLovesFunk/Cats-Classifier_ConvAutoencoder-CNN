b3bf31327801f35af6bd83913ad2e5bbe253c5f1
Clara Wicharz <clara.wicharz@gmail.com>
2023-03-18 19:31:18 +0000

    autoen plot reconst imgs in colour - result unkown

--- autoen_main.py
+++ autoen_main.py
@@ -16,6 +16,7 @@ from data_module import *
 from autoen_module import *
 
 
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.manual_seed(0)
 if device =='cuda':
@@ -30,7 +31,7 @@ if device =='cuda':
 
 # FLAGS
 
-train_flag = False
+train_flag = True
 test_flag = True
 
 
@@ -39,7 +40,7 @@ test_flag = True
 
 num_epochs = 1 ######5
 batch_size=64
-learning_rate = 0.5 ##########1e-3
+learning_rate = 1e-3 ##########
 weight_decay=1e-5
 
 supervised_ratio = 0.2
@@ -53,11 +54,12 @@ results_path = 'results/results_autoencoder.npy'
 
 train_list = glob.glob(os.path.join(train_dir,'*.jpg')) 
 
+classes = ('cat','dog')
 
 
 # LOAD AND SPLIT DATA
 
-unsupervised_list, train_list, val_list, test_list = data_split(train_list,supervised_ratio,val_ratio, test_ratio=0.001, random_state=0) ############# CHANGE TEST RATIO
+unsupervised_list, train_list, val_list, test_list = data_split(train_list,supervised_ratio,val_ratio, test_ratio=0.8, random_state=0) ############# CHANGE TEST RATIO
 
 transform_cats = transforms.Compose([   
     transforms.Resize((224, 224)),
@@ -69,13 +71,17 @@ transform_cats = transforms.Compose([
 
 
 unsupervised_data = dataset(test_list, transform=transform_cats) ######## CHANGE TEST LIST TO UNSUPERVISED LIST!!!!!!!!
+
 #print(f'unsupervised_data shape after dataset {unsupervised_data.size()}')
-data_loader= torch.utils.data.DataLoader(dataset = unsupervised_data, batch_size=batch_size, shuffle=True )
-#print(f'data_loader shape {data_loader.shape()}')
-#print(f'len(data_loader): {len(data_loader.dataset)}')
+train_loader = torch.utils.data.DataLoader(dataset = unsupervised_data, batch_size=batch_size, shuffle=True )
+test_loader = torch.utils.data.DataLoader(dataset = unsupervised_data, batch_size=batch_size, shuffle=True )
+
+
+#print(f'train_loader shape {train_loader.shape()}')
+#print(f'len(train_loader): {len(train_loader.dataset)}')
 
 # view images
-dataiter = iter(data_loader)
+dataiter = iter(train_loader)
 images, labels = dataiter.next()
 #print(f'range of values of image tensor: {torch.min(images)}, {torch.max(images)}') # based on original image values that were put in tensor and all the stuff like cropping, flipping...
 
@@ -97,99 +103,50 @@ if train_flag == True:
                                 lr=0.9,    ####################### 1e-3          
                                 weight_decay=1e-5)
 
-    model, outputs = autoen_train(num_epochs, data_loader, model, criterion, optimizer)
+    model, outputs = autoen_train(num_epochs, train_loader, model, criterion, optimizer)
     torch.save(model.state_dict(), model_path)
 
 
 
-# EVAL
+# EVAL & VIZ
 
 model = Autoencoder()
 model.load_state_dict(torch.load(model_path))
 
-
-for (img, _) in data_loader:
-    # img = img.reshape(-1, 28*28) # -> use for Autoencoder_Linear
-    
-    recon = model(img[0]) # reconstrcuted image
-    plt.imshow(  recon.permute(1, 2, 0)  ) 
-    
-    #print(recon.size())
-        #loss = criterion(recon, img) # reconstructed image vs original image and calc mean squaere error
-        
-        #optimizer.zero_grad()
-        #loss.backward()
-        #optimizer.step()
-
-    #print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
-    #outputs.append((epoch, img, recon))
-
-
-
-
-
-
-
-
-
-'''
-for (img, _) in data_loader:
-
-    # original image
-    print(f'img size in dataloader {img[0].size()}')
-    plt.imshow(  img[0].permute(1, 2, 0)  ) 
-    
-    # reconstructed image
-    recon = model(img) # reconstrcuted image
-
-#print(f'original img size {images[0].size()}')
-#plt.imshow(  images[0].permute(1, 2, 0)  ) 
-
-
-dataiter = iter(data_loader)
+#Batch of test images
+dataiter = iter(test_loader)
 images, labels = dataiter.next()
+print(labels[0])
 
-         
+#Sample outputs
+output = model(images)
+images = images.numpy()
 
-'''
+#output = output.view(batch_size, 3, 32, 32)
+output = output.detach().numpy()
 
+def imshow(img):
+    img = img / 2 + 0.5  
+    plt.imshow(np.transpose(img, (1, 2, 0))) 
 
-
-
-
-
-
-'''
-model = Autoencoder().to(device)
-model.load_state_dict(torch.load(model_path))
-
-
-
-img = outputs[0][1][0]
-recon = outputs[0][2][0]
-
-
-print(f'size original img {img.size()}')
-print(f'size reconst. img {recon.size()}')
-
-trans = torchvision.transforms.ToPILImage()
-out = trans(img)
-out.show()
-
-out_recon = trans(recon)
-out_recon.show()
+#Original Images
+print("Original Images")
+fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+for idx in np.arange(5):
+    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
+plt.show()
 
 
-#img_permute = img.permute(1, 2, 0)
-#print(f'size original img_permuted {img_permute.size()}')
-
-#plt.imshow(  img.permute(1, 2, 0)  )
-#plt.imshow(  recon.permute(1, 2, 0)  )
-
-
-
-'''
-
+#Reconstructed Images
+print('Reconstructed Images')
+fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+for idx in np.arange(5):
+    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+    imshow(output[idx])
+    ax.set_title(classes[labels[idx]])
+plt.show() 
 print('Finished Training')
 
 # %%
